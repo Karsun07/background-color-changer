077d7a6b42f87d3c309cbc9d56d7ca2e736e9d12
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-11 11:35:39 +0530

    second

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Main() {
+
+    function changeColor(color){
+        document.body.style.backgroundColor = color;
+    }
+
+    return (
+        <div>
+            <button onClick={() => changeColor("red")}>Red</button>
+            <button onClick={() => changeColor("yellow")}>Yellow</button>
+            <button onClick={() => changeColor("pink")}>Pink</button>
+        </div>
+    );
+}
+
+const parent = document.getElementById("root");
+const root = ReactDOM.createRoot(parent);
+
+root.render(<Main />);
